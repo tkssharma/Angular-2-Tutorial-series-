d10961e064439b6dd96bc55f59133a28625e4ad0
tarun <cooltarunsharma111@gmail.com>
2016-03-27 16:03:52 +0530

    chapters added

---
+++ chapter6/auction/app/components/search/search.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'auction-search',
+  templateUrl: 'app/components/search/search.html'
+})
+export default class SearchComponent {}
